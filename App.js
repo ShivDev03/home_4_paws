c3617abd1ae77ecb1eb699d819951ca56e9f0625
khanjan <khanjanraiyani4@gmail.com>
2025-03-13 23:25:24 +0530

    project init

--- App.js
+++ App.js
@@ -4,128 +4,49 @@
  *
  * @format
  */
+import React, { useEffect } from 'react';
+import { LogBox } from 'react-native';
+import MainNavigator from './src/navigation';
+import { setTopLevelNavigator } from './src/navigation/navigationsServices';
+import { setupPlayer } from './src/services/playerManager';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import './src/hooks/languages/i18next';
+import ErrorScreen from './src/services/ErrorScreen';
 import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+    backgroundNotification,
+    createChannel,
+    ForegroundMessages,
+    RequestUserPermission,
+    stopNavigationIntervals,
+} from './src/services/noitfication';
+import checkAndRequestMultiplePermissions, { PERMISSION_DATA } from './src/services/permissionManager';
+import StoreProvider from './src/services/StoreProvider';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const setNavigationRef = (navigatorRef) => {
+    setTopLevelNavigator(navigatorRef);
+};
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+LogBox.ignoreAllLogs();
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const App = () => {
+    useEffect(() => {
+        checkAndRequestMultiplePermissions(PERMISSION_DATA.ACCESS_FINE_LOCATION, ({ granted, denied }) => {});
+        RequestUserPermission();
+        createChannel();
+        ForegroundMessages();
+        backgroundNotification();
+        setupPlayer();
+        return () => stopNavigationIntervals();
+    }, []);
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
-
-  return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
+    return (
+        <>
+            <ErrorScreen>
+                <StoreProvider>
+                    <MainNavigator refer={setNavigationRef} />
+                </StoreProvider>
+            </ErrorScreen>
+        </>
+    );
+};
 export default App;
